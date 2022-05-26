57201135f84f1e2944b7e3005b25572f0f03e857
George Olufemi <georgeoluolufemi@gmail.com>
2022-05-26 20:18:44 +0100

    Embedded CSS Courses

--- src/Pages/CssCourseVideos.jsx
+++ src/Pages/CssCourseVideos.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
+import Footer from '../Components/Footer';
 import HtmlNav from '../Components/HtmlNav';
 
 function CssCourseVideos() {
   return (
     <React.Fragment>
       <HtmlNav />
-        <div>
-          csscourse
+      <div className='md:grid md:gap-5 md:grid-cols-3 px-8 md:px-16 pt-10 md:ml-12 pb-12'>
+            <div className='pb-5'>
+              <iframe width="350" height="350" src="https://www.youtube.com/embed/hu-q2zYwEYs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div className='pb-5'>
+              <iframe width="350" height="350" src="https://www.youtube.com/embed/mbeT8mpmtHA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div className='pb-5'>
+              <iframe width="350" height="350" src="https://www.youtube.com/embed/YwbIeMlxZAU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div className='pb-5'>
+              <iframe width="350" height="350" src="https://www.youtube.com/embed/D3iEE29ZXRM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div className='pb-5'>
+              <iframe width="350" height="350" src="https://www.youtube.com/embed/FHZn6706e3Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div className='pb-5'>
+              <iframe width="350" height="350" src="https://www.youtube.com/embed/kGW8Al_cga4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div className='pb-5'>
+              <iframe width="350" height="350" src="https://www.youtube.com/embed/25R1Jl5P7Mw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div className='pb-5'>
+              <iframe width="350" height="350" src="https://www.youtube.com/embed/XQaHAAXIVg8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div className='pb-5'>
+              <iframe width="350" height="350" src="https://www.youtube.com/embed/FMu2cKWD90g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div className='pb-5'>
+              <iframe width="350" height="350" src="https://www.youtube.com/embed/Xig7NsIE6DI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div className='pb-5'>
+              <iframe width="350" height="350" src="https://www.youtube.com/embed/qES0HypsUK0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
         </div>
+        <Footer />
     </React.Fragment>
   )
 }
--- src/Pages/HtmlCourseVideos.jsx
+++ src/Pages/HtmlCourseVideos.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import HtmlNav from '../Components/HtmlNav';
+import Footer from '../Components/Footer';
 
 function HtmlCourseVideos() {
   return (
     <React.Fragment>
         <HtmlNav />
-        <div className='md:grid md:gap-5 md:grid-cols-3 px-8 md:px-16 pt-10 md:ml-12'>
+        <div className='md:grid md:gap-5 md:grid-cols-3 px-8 md:px-16 pt-10 md:ml-12 pb-12'>
             <div className='pb-5'>
                 <iframe width="350" height="350" src="https://www.youtube.com/embed/Y1BlT4_c_SU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
                 </iframe>
@@ -60,6 +61,7 @@ function HtmlCourseVideos() {
                 <iframe width="350" height="350" src="https://www.youtube.com/embed/xuA8kt2xnv8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         </div>
+        <Footer />
     </React.Fragment>
   )
 }
