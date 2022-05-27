aa40eff73641acd45cf4eb1a22fa03b1bdf2db4e
George Olufemi <georgeoluolufemi@gmail.com>
2022-05-27 10:31:32 +0100

    Tried implementing dynamic animation feature

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -17,14 +17,19 @@ function Home() {
         <Navbar className='fixed' />
           {/* Hero - content goes here */}
           <div className='home h-screen bg-contain overflow-hidden'>
-            <div className="text-center">
-              <h1 className='text-white text-4xl md:text-6xl font-serif pt-64 md:pt-52 pb-10'>Coding and design courses</h1>
-              <p className='hidden md:block text-white md:text-3xl font-serif pb-10'>Learn Front End Web Development with technologies like HTML,<br /> CSS, JavaScript, React and more.</p>
-              {/* responsive for mobile devices below */}
-                <p className='md:hidden px-7 text-2xl text-white md:text-3xl font-serif pb-10'>Learn Front End Web Development with technologies like HTML, CSS, JavaScript, React and more.</p>
-                <div className="">
-                  <Link to="/courses"><button className="text-white font-serif text-xl bg-gray-800 px-6 py-3 rounded-3xl shadow-xl hover:scale-105 transition-all duration-300">View Courses</button></Link>
+            <div className=''>
+              {/* <span></span>
+              <span></span>
+              <span></span> */}
+            {/* text */}
+              <div className="text-center">
+                <h1 className='text-white text-4xl md:text-6xl font-serif pt-64 md:pt-52 pb-10'>Coding and design courses</h1>
+                <p className='hidden md:block text-white md:text-3xl font-serif pb-10'>Learn Front End Web Development with technologies like HTML,<br /> CSS, JavaScript, React and more.</p>
+                  <p className='md:hidden px-7 text-2xl text-white md:text-3xl font-serif pb-10'>Learn Front End Web Development with technologies like HTML, CSS, JavaScript, React and more.</p>
+                  <div className="">
+                    <Link to="/courses"><button className="text-white font-serif text-xl bg-gray-800 px-6 py-3 rounded-3xl shadow-xl hover:scale-105 transition-all duration-300">View Courses</button></Link>
                 </div>
+              </div>
             </div>
           </div>
           <div className='h-full overflow-hidden'>
--- src/Pages/ReactCourseVideos.jsx
+++ src/Pages/ReactCourseVideos.jsx
@@ -5,7 +5,44 @@ function ReactCourseVideos() {
   return (
     <React.Fragment>
       <HtmlNav />
-        <div>ReactCourseVideos</div>
+      <div className='md:grid md:gap-5 md:grid-cols-3 px-8 md:px-16 pt-10 md:ml-12 pb-12'>
+        <div className='pb-5'>
+          
+        </div>
+        <div className='pb-5'>
+          
+        </div>
+        <div className='pb-5'>
+          
+        </div>
+        <div className='pb-5'>
+          
+        </div>
+        <div className='pb-5'>
+          
+        </div>
+        <div className='pb-5'>
+          
+        </div>
+        <div className='pb-5'>
+          
+        </div>
+        <div className='pb-5'>
+          
+        </div>
+        <div className='pb-5'>
+          
+        </div>
+        <div className='pb-5'>
+          
+        </div>
+        <div className='pb-5'>
+          
+        </div>
+        <div className='pb-5'>
+          
+        </div>
+      </div>
     </React.Fragment>
   )
 }
