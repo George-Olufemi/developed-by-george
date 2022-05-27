84535a461d2969b86b1618d3ba71f37a5a4b6873
George Olufemi <georgeoluolufemi@gmail.com>
2022-05-27 13:35:14 +0100

    Integrated live Chat - Finalized

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,7 +10,7 @@ import html from '../html/html&css.png';
 import js from '../js/js.png';
 import react from '../react/react.png';
 import Footer from '../Components/Footer';
-import { toast, Zoom, Bounce, ToastContainer } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function Home() { 
